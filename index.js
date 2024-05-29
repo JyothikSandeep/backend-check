9da337f9d22ee1f087c8bedfff7ab9790c4c4328
JyothikSandeep <101253647+JyothikSandeep@users.noreply.github.com>
2024-05-29 16:39:12 +0530

    changes more

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const { Server } = require("socket.io");
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    // origin: "https://main--aesthetic-parfait-7d40d0.netlify.app",
-    origin: "http://localhost:3000",
+    origin: "https://main--aesthetic-parfait-7d40d0.netlify.app",
+    // origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
